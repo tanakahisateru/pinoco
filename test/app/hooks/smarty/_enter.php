3724351a7a3fa43745d19959dd5a3b64fd704c5b
Hisateru Tanaka <tanakahisateru@gmail.com>
2009-09-09 14:11:35 +0000

    Avoid bug of Smarty under 2.6.25

--- test/app/hooks/smarty/_enter.php
+++ test/app/hooks/smarty/_enter.php
@@ -2,7 +2,9 @@
 // How to extend Pinoco page renderer.
 $this->renderers->tpl = $this->newobj('smarty_renderer.php/Pinoco_SmartyRenderer', $this);
 $this->renderers->tpl->cfg->compile_dir = $this->sysdir . "/tmp";
+session_start(); // avoid bug of Smarty under 2.6.25
 
 // Trick: The extension html is switched to tpl.
 $this->page = preg_match('/\/$/', $this->path) ?
     ($this->path . 'index.tpl') : preg_replace('/(.*)\.html$/', '${1}.tpl', $this->path);
+
