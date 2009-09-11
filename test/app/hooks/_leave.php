4509d065795a1d88363f6507a7dc00c9a009be2f
Hisateru Tanaka <tanakahisateru@gmail.com>
2009-09-11 21:28:02 +0000

    Performance optimizing (test)

--- test/app/hooks/_leave.php
+++ test/app/hooks/_leave.php
@@ -1,6 +1,9 @@
 <?php
 // How to show status after page rendering (buffering must be enabled)
-if(0 && $this->using('FirePHPCore/FirePHP.class.php')) {
+if(!preg_match('/\sFirePHP\//', $_SERVER['HTTP_USER_AGENT'])) {
+    $this->skip(); // cancel this script here
+}
+if($this->using('FirePHPCore/FirePHP.class.php')) {
     $firephp = FirePHP::getInstance(true);
     $firephp->group('Pinoco activity', array('Collapsed'=>1));
     foreach($this->activity as $act) {
