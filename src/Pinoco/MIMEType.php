f1b9d5285f33a5f6bf8dcd9fded9bf6b9a95e502
Hisateru Tanaka <tanakahisateru@gmail.com>
2009-10-14 14:12:16 +0000

    Packaging

--- src/Pinoco/MIMEType.php
+++ src/Pinoco/MIMEType.php
@@ -9,7 +9,7 @@
  * @package  Pinoco
  * @author   Hisateru Tanaka <tanakahisateru@gmail.com>
  * @license  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
- * @version  0.1.0
+ * @version  0.1.1
  * @link     http://code.google.com/p/pinoco/
  * @filesource
  */
