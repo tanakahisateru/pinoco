ac1b696d4efb446d47a7aa3f4cd153575131ced2
Hisateru Tanaka <tanakahisateru@gmail.com>
2010-03-04 19:26:10 +0000

    Add a home link to error pages.

--- tutorial/17.customerror/app/error/404.php
+++ tutorial/17.customerror/app/error/404.php
@@ -1,4 +1,5 @@
 <div align="center">
 <h1>Sorry, we don't have that.</h1>
 <p><?php echo $message;?></p>
+<p><a href="<?php echo $this->url("/");?>">Go back to home</a></p>
 </div>
--- tutorial/17.customerror/app/error/default.php
+++ tutorial/17.customerror/app/error/default.php
@@ -1,5 +1,6 @@
 <div align="center">
 <h1><?php echo $code;?> <?php echo $title;?></h1>
 <p><?php echo $message;?></p>
-<p>Please report it to: xxx@xxx.com</p>
+<p>Please report to: xxx@xxx.com</p>
+<p><a href="<?php echo $this->url("/");?>">Go back to home</a></p>
 </div>
