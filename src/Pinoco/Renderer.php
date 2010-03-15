6fb4ea6066435296ef6ae2e3f7f7561018efaf5b
Hisateru Tanaka <tanakahisateru@gmail.com>
2010-03-15 16:25:52 +0000

    Release coordinate for 0.2.1

--- src/Pinoco/Renderer.php
+++ src/Pinoco/Renderer.php
@@ -9,7 +9,7 @@
  * @package  Pinoco
  * @author   Hisateru Tanaka <tanakahisateru@gmail.com>
  * @license  http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
- * @version  0.2.0
+ * @version  0.2.1
  * @link     http://code.google.com/p/pinoco/
  * @filesource
  */
