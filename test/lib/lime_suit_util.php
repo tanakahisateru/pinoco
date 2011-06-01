4aae78ab77e23231f7ce718df2c23beb7e996919
Hisateru Tanaka <tanakahisateru@gmail.com>
2011-06-01 14:25:37 +0900

    Each tests should be executed in its own php environment.

--- test/lib/lime_suit_util.php
+++ test/lib/lime_suit_util.php
@@ -1,68 +1,19 @@
 <?php
-require_once dirname(__FILE__) . '/lime.php';
-
-function _run_unit_test_innner($test)
-{
-    include $test;
-    
-    $all_results = lime_test::to_array();
-    $numtests = count($all_results);
-    lime_test::reset();
-    for($i = 0; $i < $numtests; $i++) {
-        restore_error_handler();
-        restore_exception_handler();
-    }
-    
-    return $all_results;
-}
-
 function run_unit_test($test, $display_each_result=false)
 {
     echo $test . ":\n";
-    if(!$display_each_result) {
-        ob_start();
+    $p = popen('php ' . $test, 'r');
+    $buf = "";
+    while($b = fread($p, 1024)) {
+        $buf .= $b;
     }
-    $all_results = _run_unit_test_innner($test);
-    if(!$display_each_result) {
-        ob_end_clean();
-    }
-    foreach($all_results as $results) {
-        $passed = $results['stats']['passed'];
-        $skipped = $results['stats']['skipped'];
-        $failed = $results['stats']['failed'];
-        $total = $results['stats']['total'];
-        printf("%d/%d tests succeseeded.", count($passed), $total);
-        if(!empty($skipped)) {
-            printf(" (%d tests skipped.)", count($skipped));
+    fclose($p);
+    $rs = explode("\n", $buf);
+    foreach($rs as $r) {
+        if(!$display_each_result && preg_match('/^ok/i', $r)) {
+            continue;
         }
-        echo "\n";
-        
-        foreach($failed as $n) {
-            $message = $results['tests'][$n]['message'];
-            $status = $results['tests'][$n]['status'];
-            printf("not ok %d%s\n", $n, $message);
-            printf('    Failed test (%s at line %d)',
-                str_replace(getcwd(), '.', $results['tests'][$n]['file']),
-                $results['tests'][$n]['line']
-            );
-            echo "\n";
-            if($results['tests'][$n]['error']) {
-                echo $results['tests'][$n]['error'];
-                echo "\n";
-            }
-        }
-        
-        foreach($skipped as $n) {
-            $message = $results['tests'][$n]['message'];
-            $status = $results['tests'][$n]['status'];
-            printf("skipped %d%s\n", $n, $message);
-            printf('    Skipped test (%s at line %d)',
-                str_replace(getcwd(), '.', $results['tests'][$n]['file']),
-                $results['tests'][$n]['line']
-            );
-            echo "\n";
-        }
-        echo "\n";
+        echo $r . "\n";
     }
 }
 
