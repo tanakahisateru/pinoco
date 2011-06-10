4d13266054bfdd3258416469ca17027a7903eaab
Hisateru Tanaka <tanakahisateru@gmail.com>
2011-06-11 02:34:18 +0900

    Namespace enhancement test

--- sandbox/app/hooks/_enter.php
+++ sandbox/app/hooks/_enter.php
@@ -5,7 +5,7 @@ $this->renderers->html->cfg->encoding = "UTF-8";
 
 header("Content-Type:text/html;charset=utf-8");
 
-$this->autolocal->title = sprintf("Pinoco Test (%s)", basename(dirname($_SERVER['SCRIPT_NAME'])));
+$this->autolocal->title = sprintf("Pinoco Test\n(%s)", basename(dirname($_SERVER['SCRIPT_NAME'])));
 
 //echo "<html>";
 //trigger_error("My error", E_USER_WARNING);
