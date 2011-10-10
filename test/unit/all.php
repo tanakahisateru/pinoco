d2dcc3e7b49ec4da200a658548b2af5a6f5e71b5
Hisateru Tanaka <tanakahisateru@gmail.com>
2011-10-11 00:11:44 +0900

    Testing framework changed to PHPUnit.

--- test/unit/all.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/../lib/lime_suit_util.php';
-run_unit_test(dirname(__FILE__) . '/test_vars.php');
-run_unit_test(dirname(__FILE__) . '/test_list.php');
-run_unit_test(dirname(__FILE__) . '/test_lazy.php');
-run_unit_test(dirname(__FILE__) . '/test_pdo.php');
-run_unit_test(dirname(__FILE__) . '/test_validator.php');
-run_unit_test(dirname(__FILE__) . '/test_pinoco.php');
-
