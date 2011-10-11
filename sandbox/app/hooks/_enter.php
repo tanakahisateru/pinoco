582c4fcac815759d52284af2030c00186122aa63
Hisateru Tanaka <tanakahisateru@gmail.com>
2011-10-11 13:34:52 +0900

    Test for #37

--- sandbox/app/hooks/_enter.php
+++ sandbox/app/hooks/_enter.php
@@ -1,7 +1,11 @@
 <?php
-$this->renderers->html->cfg->phpCodeDestination = $this->sysdir . "/tmp";
-$this->renderers->html->cfg->encoding = "UTF-8";
-//$this->renderers->html->cfg->outputMode = 11;  // XHTML=11, XML=22, HTML5=55
+function setupTALRenderer($renderer) {
+    $pinoco = Pinoco::instance();
+    $renderer->cfg->phpCodeDestination = $pinoco->sysdir . "/tmp";
+    $renderer->cfg->encoding = "UTF-8";
+    //$renderer->cfg->outputMode = 11;  // XHTML=11, XML=22, HTML5=55
+}
+$this->renderers->html->before_rendering = 'setupTALRenderer';
 
 header("Content-Type:text/html;charset=utf-8");
 
