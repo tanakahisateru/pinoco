bafc8479133203d089fcca96305f9169620969c6
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-01-16 13:42:20 +0900

    Project structure changed: sandbox moved into test.

--- test/sandbox/www/basic/_gateway.php
+++ test/sandbox/www/basic/_gateway.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 //TEST:
 if(function_exists('xdebug_disable')){ xdebug_disable(); }
 
-require_once '../../../src/Pinoco.php';
+require_once '../../../../src/Pinoco.php';
 $pinoco = Pinoco::create("../../app", array(
 //    'use_mod_rewrite'  => TRUE,  // TRUE or FALSE default TRUE
 //    'use_path_info'    => TRUE,  // TRUE or FALSE default TRUE
