892b7a7dee7902217190c39508879f4e0caf1d3e
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-01-16 20:31:42 +0900

    Empty project sample follows credit change and config api.

--- empty-project/htdocs/_gateway.php
+++ empty-project/htdocs/_gateway.php
@@ -1,9 +1,12 @@
 <?php
-require_once 'Pinoco.php';
-Pinoco::create("../app", array(
+require_once '_app/lib/Pinoco.php';
+Pinoco::credit_into_header();
+Pinoco::create("_app", array(
 //    'use_mod_rewrite'  => TRUE,  // TRUE or FALSE default TRUE
 //    'use_path_info'    => TRUE,  // TRUE or FALSE default TRUE
 //    'custom_path_info' => FALSE, // FALSE(auto) or string default FALSE
 //    'directory_index'  => "index.html index.php", // string like DirectoryIndex directive default "index.html index.php"
-))->run();
+))
+->config('config', 'config.ini')
+->run();
 
