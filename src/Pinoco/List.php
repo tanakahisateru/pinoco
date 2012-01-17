824b394a43a03db9a01c0286843c9bbfbcbfce5c
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-01-17 23:42:14 +0900

    Remove unnecessary doc comments.

--- src/Pinoco/List.php
+++ src/Pinoco/List.php
@@ -1,20 +1,16 @@
 <?php
 /**
  * Pinoco: makes existing static web site dynamic transparently.
- * Copyright 2010-2011, Hisateru Tanaka <tanakahisateru@gmail.com>
+ * Copyright 2010-2012, Hisateru Tanaka <tanakahisateru@gmail.com>
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
  * PHP Version 5
  *
- * @category   Framework
  * @author     Hisateru Tanaka <tanakahisateru@gmail.com>
- * @copyright  Copyright 2010-2011, Hisateru Tanaka <tanakahisateru@gmail.com>
+ * @copyright  Copyright 2010-2012, Hisateru Tanaka <tanakahisateru@gmail.com>
  * @license    MIT License (http://www.opensource.org/licenses/mit-license.php)
- * @version    0.5.2
- * @link       https://github.com/tanakahisateru/pinoco
- * @filesource
  * @package    Pinoco
  */
 
