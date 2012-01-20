8b396b26224f865804a6e3c3686dc09f1870f8ae
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-01-20 14:56:38 +0900

    Remove htaccess tricks from all tutorials.

--- tutorial/12.autolocal/htdocs/_gateway.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Pinoco.php';
-Pinoco::create("../app", array(
-//    'use_mod_rewrite'  => TRUE,  // TRUE or FALSE default TRUE
-//    'use_path_info'    => TRUE,  // TRUE or FALSE default TRUE
-//    'custom_path_info' => FALSE, // FALSE(auto) or string default FALSE
-//    'directory_index'  => "index.html index.php", // string like DirectoryIndex directive default "index.html index.php"
-))->run();
-
