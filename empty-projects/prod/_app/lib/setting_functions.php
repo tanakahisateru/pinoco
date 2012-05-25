8720cb6f2cd1f0d6f811c300a3c3c0bfbce7d0c3
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-05-25 14:38:31 +0900

    Supporting both of autoloader and explicit require.

--- empty-projects/prod/_app/lib/setting_functions.php
+++ empty-projects/prod/_app/lib/setting_functions.php
@@ -1,8 +1,9 @@
 <?php
 function setupTALRenderer($renderer) {
-    require_once 'PHPTAL.php';
+    if(!class_exists('PHPTAL')) {
+        require_once 'PHPTAL.php';
+    }
     $renderer->cfg->phpCodeDestination = Pinoco::instance()->sysdir . "/cache";
     $renderer->cfg->encoding = "UTF-8";
     $renderer->cfg->outputMode = PHPTAL::XHTML;  // XHTML, XML or HTML5
 }
-
