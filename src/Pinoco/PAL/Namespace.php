51672b9a836047224ec5934a9aa6382e84c6bc86
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-06-26 03:23:51 +0900

    Clean doc comments for PAL package.

--- src/Pinoco/PAL/Namespace.php
+++ src/Pinoco/PAL/Namespace.php
@@ -8,13 +8,9 @@
  *
  * PHP Version 5
  *
- * @category   Framework
  * @author     Hisateru Tanaka <tanakahisateru@gmail.com>
  * @copyright  Copyright 2010-2011, Hisateru Tanaka <tanakahisateru@gmail.com>
  * @license    MIT License (http://www.opensource.org/licenses/mit-license.php)
- * @version    0.5.2
- * @link       https://github.com/tanakahisateru/pinoco
- * @filesource
  * @package    Pinoco
  * @subpackage PAL
  */
