ec5b1351fb9b0b72dc334a88fd8515a3b9d99ee0
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-09-15 01:01:13 +0900

    Require order in empty projects fixed.

--- empty-projects/min/_gateway.php
+++ empty-projects/min/_gateway.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../../src/Pinoco.php';
 if (file_exists('_app/vendor/autoload.php')) { require '_app/vendor/autoload.php'; }
+if (!class_exists('Pinoco')) { require_once '_app/lib/Pinoco.php'; }
 
 Pinoco::creditIntoHeader();
 Pinoco::create("_app")->run();
