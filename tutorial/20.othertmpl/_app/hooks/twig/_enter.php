c1efa91fa598ecec21ddeb97773b3ba5b9e6c91c
Hisateru Tanaka <tanakahisateru@gmail.com>
2013-01-28 18:38:17 +0900

    Let Twig template end with .html.twig

--- tutorial/20.othertmpl/_app/hooks/twig/_enter.php
+++ tutorial/20.othertmpl/_app/hooks/twig/_enter.php
@@ -1,5 +1,16 @@
 <?php
+require_once "TwigRenderer.php";
+
 $this->page_modifier = null; // reset
 
-require_once "TwigRenderer.php";
-$this->renderers->html = new TwigRenderer($this);
+function page_ext_html2twig($path)
+{
+	if (preg_match('/\/$/', $path)) {
+		return $path . 'index.html.twig';
+	} else {
+		return preg_replace('/(.*)\.html$/', '${1}.html.twig', $path);
+	}
+}
+$this->page_modifier = 'page_ext_html2twig';
+
+$this->renderers->twig = new TwigRenderer($this);
