3ffd126b3987eacb4ee9164e19a5f96f8598eef4
Hisateru Tanaka <tanakahisateru@gmail.com>
2013-04-12 19:15:23 +0900

    Bump up version to 0.7.2.

--- src/Pinoco.php
+++ src/Pinoco.php
@@ -70,7 +70,7 @@ require_once(dirname(__FILE__) . '/Pinoco/_bootstrap.php');
  */
 class Pinoco extends Pinoco_DynamicVars
 {
-    const VERSION = "0.7.1";
+    const VERSION = "0.7.2";
 
     private $_baseuri;   // R gateway index.php location on internet
     private $_basedir;   // R gateway index.php location on file system
